ac385578f99bc85bb91b945e22312654dcbf89a2
harishkumarm21 <harishkumarm2110@gmail.com>
2026-07-14 09:21:01 +0530

    login phase 1 tested

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -4,30 +4,20 @@ import { sendSuccess } from "../../utils/api-response.js";
 
 export class AuthController {
   async register(req: Request, res: Response, next: NextFunction) {
-    try {
+    const user = await authService.register(req.body)
 
-      const user = await authService.register(req.body)
-
-      return sendSuccess({
-        res,
-        statusCode: 201,
-        message: "User registered successfully.",
-        data: user,
-      });
-
-    } catch (error) {
-      next(error)
-    }
+    return sendSuccess({
+      res,
+      statusCode: 201,
+      message: "User registered successfully.",
+      data: user,
+    });
   }
 
   async login(req: Request, res: Response, next: NextFunction) {
-    try {
-      const user = await authService.login(req.body);
+    const user = await authService.login(req.body);
 
-      sendSuccess({ res, statusCode: 200, message: "Login Successfully", data: user })
-    } catch (error) {
-      next(error)
-    }
+    sendSuccess({ res, statusCode: 200, message: "Login Successfully", data: user })
   }
 }
 
